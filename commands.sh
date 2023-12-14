18809114203266bd628ca4b470d0b81a52e76854
Topor Karol <toporkarol@gmail.com>
2023-12-14 02:00:44 +0100

    implemented node monitoring in docker

--- commands.sh
+++ commands.sh
@@ -1,4 +1,6 @@
 # get containers up
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' grafana
 
+# create network cutom_bridge
+docker network create --driver bridge --subnet 172.20.0.0/16 custom_bridge
 
