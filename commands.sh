84dd5d1d8f263936e65137c09d0a5e8c1f4afff9
Topor Karol <toporkarol@gmail.com>
2023-12-29 10:47:18 +0100

    stuff

--- commands.sh
+++ commands.sh
@@ -4,3 +4,24 @@ docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' gra
 # create network cutom_bridge
 docker network create --driver bridge --subnet 172.20.0.0/16 custom_bridge
 
+# build image
+# troubleshooting container
+docker build -t debug-tools .
+docker run -it --rm --network custom_bridge debug-tools
+
+# prometheus
+docker build -t local/prometheus:1.0 prometheus/.
+# jmx-exporter
+docker build -t local/jmx-exporter:1.11 jmx-exporter/.
+
+
+# enter docker container
+docker run --rm -it -v ./jmx-exporter:/config local/jmx-exporter:1.8 /bin/sh
+docker run --rm -it --entrypoint /bin/sh prom/prometheus:latest -c whoami
+
+# remove all not running containers
+docker rm $(docker ps -a -q -f "status=exited")
+
+# permissions
+chown toporek3112:docker_container prometheus/ && chmod -R 764 prometheus/
+
---
+++ dockerfile
@@ -0,0 +1,18 @@
+# Use OpenJDK 11 JDK base image
+FROM openjdk:11-jdk
+
+# Install essential tools
+RUN apt-get update && apt-get install -y \
+    net-tools \
+    procps \
+    curl \
+    && rm -rf /var/lib/apt/lists/*
+
+# Download jmxterm
+RUN curl -L -o /root/jmxterm-1.0.2-uber.jar https://github.com/jiaqi/jmxterm/releases/download/v1.0.2/jmxterm-1.0.2-uber.jar
+
+# Set the working directory
+WORKDIR /root
+
+# By default, just run a shell. Users can execute Java tools or other commands as needed.
+CMD ["/bin/bash"]
